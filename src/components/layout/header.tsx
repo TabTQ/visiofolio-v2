b4705931d7efa34733c26177f0a47483c5e5ad2a
Tabish Tariq <tabishtariqaz@gmail.com>
2025-05-06 16:37:32 +0000

    increase the profile picture size

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -61,7 +61,7 @@ export const Header = () => {
             </a>
           )}
           {profilePicture && (
-            <Avatar className="h-14 w-14 ml-2 border-2 border-primary-foreground/50"> {/* Increased size */}
+            <Avatar className="h-16 w-16 ml-2 border-2 border-primary-foreground/50"> {/* Increased size */}
               <AvatarImage src={profilePicture} alt={name} data-ai-hint={profilePictureHint} />
               <AvatarFallback>{name.substring(0, 1)}</AvatarFallback>
             </Avatar>
